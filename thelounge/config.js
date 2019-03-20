e3e57aeadf5c930e73a12273bac577bd07aaf669
Alistair McKinlay <github@10people.co.uk>
2019-03-20 16:08:08 +0000

    Don't bind thelounge to localhost, that breaks nginx

--- thelounge/config.js
+++ thelounge/config.js
@@ -26,7 +26,7 @@ module.exports = {
 	// For UNIX domain sockets, use `"unix:/absolute/path/to/file.sock"`.
 	//
 	// This value is set to `undefined` by default to listen on all interfaces.
-	host: "127.0.0.1",
+	host: undefined,
 
 	// ### `port`
 	//
