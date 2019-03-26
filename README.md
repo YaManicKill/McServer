15a4193d570df1f605447439bdffb86b3322b177
Alistair McKinlay <github@10people.co.uk>
2019-03-26 09:03:25 +0000

    Change how we deal with secrets

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ I'm using docker for this.
 ## Usage
 
 * Set up a server using an Alpine Linux image
-* Copy all files onto it
-* Ensure you are in this root working directory
-* sh ./set-up-host.sh DIGITAL_OCEAN_KEY # Where DIGITAL_OCEAN_KEY is your digital ocean API key
+* Copy this directory onto the server
+* Copy a secrets.tar.gz with files in the same structure as this directory into the directory
+    * As a minimum, this should include `acme/conf/credentials` file with `dns_digitalocean_token = TOKEN`, where TOKEN is your digital ocean API key
+    * This should also include files such as `znc/users/*/networks/*/moddata/nickserv` files and `thelounge/users/*.json`
+* `cd` into the directory
+* sh ./set-up-host.sh
--- set-up-host.sh
+++ set-up-host.sh
@@ -1,5 +1,4 @@
 # Script to set up the host
-# Takes 1 argument, the digital ocean api key
 # Should only ever be run once on a machine
 
 # WARNING: This still needs tested!!!!
@@ -14,12 +13,12 @@ adduser -D al
 echo 'al ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 mkdir ~al/.ssh
 cp ~/.ssh/authorized_keys ~al/.ssh/
+
+tar -xf secrets.tar.gz
+
 # Secure ssh
 sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config # Disable password login via ssh
 sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config # Disable root login via ssh
-
-# Save digital ocean API key for initial ssl certs
-echo "dns_digitalocean_token = $1" > acme/conf/credentials
 
 # Get initial SSL certs
 docker run -it --rm --name certbot -v "${PWD}/acme/certs:/etc/letsencrypt" -v "${PWD}/acme/conf:/opt/certbot/conf" certbot/dns-digitalocean certonly --dns-digitalocean --dns-digitalocean-credentials conf/credentials -d *.yamanickill.com -d yamanickill.com -d mckinlay.me -d *.mckinlay.me -d 10people.co.uk -d *.10people.co.uk --server https://acme-v02.api.letsencrypt.org/directory -m "certbot@10people.co.uk" --agree-tos --no-eff-email
