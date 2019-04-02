a968533893468fe5cfdf5c643ae21b9ab75fda84
Alistair McKinlay <github@10people.co.uk>
2019-04-02 11:07:04 +0100

    Update setup script to support initial sites, rename, and correct renewal

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ I'm using docker for this.
 
 * Set up a server using an Alpine Linux image
 * Copy this directory onto the server
-* Copy a secrets.tar.gz with files in the same structure as this directory into the directory
+* Copy a `secrets.tar.gz` with files in the same structure as this directory into the directory
     * As a minimum, this should include `acme/conf/credentials` file with `dns_digitalocean_token = TOKEN`, where TOKEN is your digital ocean API key
     * This should also include files such as `znc/users/*/networks/*/moddata/nickserv` files and `thelounge/users/*.json`
+* Copy over the initial state of the static sites as `sites.tar.gz`
 * `cd` into the directory
 * sh ./set-up-host.sh
--- set-up-host.sh
+++ set-up-host.sh
@@ -13,18 +13,20 @@ adduser -D al
 echo 'al ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 mkdir ~al/.ssh
 cp ~/.ssh/authorized_keys ~al/.ssh/
+chown -R al:al ~al
 
 tar -xf secrets.tar.gz
+tar -xf sites.tar.gz nginx/data/
 
 # Secure ssh
 sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config # Disable password login via ssh
 sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config # Disable root login via ssh
 
 # Get initial SSL certs
-docker run -it --rm --name certbot -v "${PWD}/acme/certs:/etc/letsencrypt" -v "${PWD}/acme/conf:/opt/certbot/conf" certbot/dns-digitalocean certonly --dns-digitalocean --dns-digitalocean-credentials conf/credentials -d *.yamanickill.com -d yamanickill.com -d mckinlay.me -d *.mckinlay.me -d 10people.co.uk -d *.10people.co.uk -d harvestseason.club -d *.harvestseason.club -d podcastdrivendev.com -d *.podcastdrivendev.com -d rantswithal.com -d *.rantswithal.com --server https://acme-v02.api.letsencrypt.org/directory -m "certbot@10people.co.uk" --agree-tos --no-eff-email
+docker run -it --rm --name certbot -v "${PWD}/acme/certs:/etc/letsencrypt" -v "${PWD}/acme/conf:/opt/certbot/conf" certbot/dns-digitalocean certonly --dns-digitalocean --dns-digitalocean-credentials conf/credentials -d *.yamanickill.com -d yamanickill.com -d mckinlay.me -d *.mckinlay.me -d 10people.co.uk -d *.10people.co.uk -d harvestseason.club -d *.harvestseason.club -d podcastdrivendev.com -d *.podcastdrivendev.com -d rantswithal.com -d *.rantswithal.com -d mckinlays.me -d *.mckinlays.me --server https://acme-v02.api.letsencrypt.org/directory -m "certbot@10people.co.uk" --agree-tos --no-eff-email
 
 # Add ssl cert renew command to cron (currently set to every 7 days)
-renewCommand = "docker run -it --rm --name certbot -v '${PWD}/certs:/etc/letsencrypt' -v '${PWD}/conf:/opt/certbot/conf' certbot/dns-digitalocean renew --force-renewal"
+renewCommand = "docker run -it --rm --name certbot -v '${PWD}/certs:/etc/letsencrypt' -v '${PWD}/conf:/opt/certbot/conf' certbot/dns-digitalocean renew"
 (crontab -l 2>/dev/null; echo "0 0 */7 * * $renewCommand") | crontab -
 
 # Delete credential file for ssl cert, we no longer need it as certbot saved it
